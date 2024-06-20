23faf3dc99a75cc0034b931724ef0a0f0b1da01d
Andrei Tsitsilin <tsitsilinav@gmail.com>
2024-06-20 22:45:34 +0300

    Добавил генератор модели базы данных

---
+++ webapp/config.py
@@ -0,0 +1,10 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+WEATHER_DEFAULT_CITY = "Roston na Donu,Russia"
+WEATHER_API_KEY = "f4feafc05c2c4c36b41105301240206"
+WEATHER_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+SQLALCHEMY_DATABASE_URI ='sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
